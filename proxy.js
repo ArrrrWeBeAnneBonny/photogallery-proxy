f37ec8829159ece6e7e793acfed8a13b1833a87f
Michael Gallien <Michael.Gallien2@gmail.com>
2021-05-22 14:05:26 -0700

    Proxy Finished

--- proxy.js
+++ proxy.js
@@ -27,7 +27,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = 4000;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('/users/michaelgallien/HackReactor/FEC/photogallery-proxy'));
